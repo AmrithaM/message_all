651535fef1318223004fc467c0f1529aff2e5b32
Amritha Marimuthu <amritham93@gmail.com>
2020-08-13 19:48:18 -0400

    Build User Resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Siddharth", password: "password")
+User.create(username: "Magizhan", password: "password")
+User.create(username: "Chandler", password: "password")
+User.create(username: "Arya", password: "password")
+User.create(username: "Joey", password: "password")
